f5ab21c07735c53c129b0c2ccd2ba745d3febeb8
Ying Li <94812362+YingLi-pku@users.noreply.github.com>
2025-05-01 10:40:27 +0800

    Update GWAS.R

--- SimulationCode/GWAS.R
+++ SimulationCode/GWAS.R
@@ -5,13 +5,6 @@ print(args)
 print(sessionInfo())
 n.cpu=as.numeric(args)
 ##########################################
-library(survival)
-library(ggplot2)
-library(patchwork)
-library(reshape2)
-library(data.table)
-library(tidyr)
-library(dplyr)
 library(WtCoxG)
 ###################################################################################################
 params = expand.grid(prev = c(0.01,0.05,0.1)                     # disease prevalence
